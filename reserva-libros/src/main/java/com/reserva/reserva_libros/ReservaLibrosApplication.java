16c4ae71a529d093a1f1b29e0505cb4ef552d080
Adrián Vega <adrian.vegaf14@gmail.com>
2025-06-24 21:47:35 -0500

    feat: proyecto monolito de reserva de libros

---
+++ reserva-libros/src/main/java/com/reserva/reserva_libros/ReservaLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.reserva.reserva_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaLibrosApplication.class, args);
+	}
+
+}
---
+++ reserva-libros/src/test/java/com/reserva/reserva_libros/ReservaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserva.reserva_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
